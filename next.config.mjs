4c963ac816392b662897ca0a280f7a14f029f427
awashimakentaro <awashimanman@gmail.com>
2025-10-15 01:29:59 +0900

    renderに切り替えるためにnext.config.mjsを編集

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-}
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  images: { unoptimized: true },
 
-export default nextConfig
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://campus-qiita-backend.onrender.com/:path*',
+      },
+    ];
+  },
+};
+
+export default nextConfig;
